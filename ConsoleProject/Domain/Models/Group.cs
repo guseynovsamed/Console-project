9f2231cc856f4977917bdfca7fd099038b470014
guseynovsamed <samadaghash@code.edu.az>
2023-11-19 21:06:41 +0400

    Main details created

---
+++ ConsoleProject/Domain/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Models
+{
+    public class Group : BaseEntity
+    {
+        public string Name { get; set; }
+        public  int Capacity { get; set; }
+    }
+}
